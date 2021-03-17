d3c85b30e3005274353994ee6bef7d553be95caa
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2021-03-17 01:21:40 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
   def new 
   end 
   
-  def create
+def create
   Post.create(title: params[:post][:title], description: params[:post][:description])
   redirect_to posts_path
 end
